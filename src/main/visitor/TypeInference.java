574d39528c8238915e16d050954b1fde89bf5339
mrazimi99 <mrazimi99@gmail.com>
2021-06-01 03:05:04 +0430

    Add type inference rules for binary expressions.

--- src/main/visitor/TypeInference.java
+++ src/main/visitor/TypeInference.java
@@ -17,90 +17,126 @@ import main.symbolTable.items.*;
 import java.util.*;
 
 public class TypeInference extends Visitor<Type> {
-    @Override
-    public Type visit(BinaryExpression binaryExpression) {
-        Expression left = binaryExpression.getFirstOperand();
-        Expression right = binaryExpression.getSecondOperand();
+	@Override
+	public Type visit(BinaryExpression binaryExpression) {
+		Expression left = binaryExpression.getFirstOperand();
+		Expression right = binaryExpression.getSecondOperand();
 
-        Type tl = left.accept(this);
-        Type tr = right.accept(this);
-        BinaryOperator operator =  binaryExpression.getBinaryOperator();
+		Type tl = left.accept(this);
+		Type tr = right.accept(this);
+		BinaryOperator operator =  binaryExpression.getBinaryOperator();
 
+		if (operator.equals(BinaryOperator.and) || operator.equals(BinaryOperator.or)) {
+			if (tl instanceof BoolType && tr instanceof BoolType)
+				return new BoolType();
 
-        if (operator.equals(BinaryOperator.and) || operator.equals(BinaryOperator.or)){
-            if (tl instanceof BoolType && tr instanceof BoolType)
-                return new BoolType();
+			if ((tl instanceof NoType || tl instanceof BoolType) && (tr instanceof BoolType || tr instanceof NoType))
+				return new NoType();
+		}
+		else if (operator.equals(BinaryOperator.add) || operator.equals(BinaryOperator.sub) || operator.equals(BinaryOperator.mult) || operator.equals(BinaryOperator.div) || operator.equals(BinaryOperator.gt) || operator.equals(BinaryOperator.lt)) {
+			if (tl instanceof IntType && tr instanceof IntType)
+				return new IntType();
 
-            if ((tl instanceof NoType || tl instanceof BoolType) && (tr instanceof BoolType || tr instanceof NoType))
-                return new NoType();
-        }
-        //TODO
-        return new NoType();
-    }
+			if ((tl instanceof NoType || tl instanceof IntType) && (tr instanceof IntType || tr instanceof NoType))
+				return new NoType();
+		}
+		else if (operator.equals(BinaryOperator.eq) || operator.equals(BinaryOperator.neq)) {
+			if (!(tl instanceof ListType || tr instanceof ListType)) {
 
-    @Override
-    public Type visit(UnaryExpression unaryExpression) {
-        //TODO
-        return null;
-    }
+				if (tl instanceof NoType || tr instanceof NoType)
+					return new NoType();
 
-    @Override
-    public Type visit(AnonymousFunction anonymousFunction) {
-        //TODO
-        return null;
-    }
+				if (tl.getClass().equals(tr.getClass()))
+					return new BoolType();
 
-    @Override
-    public Type visit(Identifier identifier) {
-        //TODO
-        return null;
-    }
+				if ((tl instanceof FptrType && tr instanceof VoidType) || (tl instanceof VoidType && tr instanceof FptrType))
+					return new BoolType();
+			}
+		}
+		else if (operator.equals(BinaryOperator.append)) {
+			if (tl instanceof ListType) {
+				if (tr instanceof NoType)
+					return new NoType();
 
-    @Override
-    public Type visit(ListAccessByIndex listAccessByIndex) {
-        //TODO
-        return null;
-    }
+				if (tr.getClass().equals(((ListType)tl).getType()))
+					return new ListType(((ListType)tl).getType());
 
-    @Override
-    public Type visit(ListSize listSize) {
-        //TODO
-        return null;
-    }
+				if (tr instanceof FptrType && tl instanceof VoidType)
+					return new ListType(((ListType)tl).getType());
+			}
+			else if (tl instanceof NoType) {
+				return new NoType();
+			}
+		}
 
-    @Override
-    public Type visit(FunctionCall funcCall) {
-        //TODO
-        return null;
-    }
+		UnsupportedOperandType error = new UnsupportedOperandType(binaryExpression.getLine(), operator.name());
+		binaryExpression.addError(error);
+		return new NoType();
+	}
 
-    @Override
-    public Type visit(ListValue listValue) {
-        //TODO
-        return null;
-    }
+	@Override
+	public Type visit(UnaryExpression unaryExpression) {
+		//TODO
+		return null;
+	}
 
-    @Override
-    public Type visit(IntValue intValue) {
-        //TODO
-        return null;
-    }
+	@Override
+	public Type visit(AnonymousFunction anonymousFunction) {
+		//TODO
+		return null;
+	}
 
-    @Override
-    public Type visit(BoolValue boolValue) {
-        //TODO
-        return null;
-    }
+	@Override
+	public Type visit(Identifier identifier) {
+		//TODO
+		return null;
+	}
 
-    @Override
-    public Type visit(StringValue stringValue) {
-        //TODO
-        return null;
-    }
+	@Override
+	public Type visit(ListAccessByIndex listAccessByIndex) {
+		//TODO
+		return null;
+	}
 
-    @Override
-    public Type visit(VoidValue voidValue) {
-        //TODO
-        return null;
-    }
+	@Override
+	public Type visit(ListSize listSize) {
+		//TODO
+		return null;
+	}
+
+	@Override
+	public Type visit(FunctionCall funcCall) {
+		//TODO
+		return null;
+	}
+
+	@Override
+	public Type visit(ListValue listValue) {
+		//TODO
+		return null;
+	}
+
+	@Override
+	public Type visit(IntValue intValue) {
+		//TODO
+		return null;
+	}
+
+	@Override
+	public Type visit(BoolValue boolValue) {
+		//TODO
+		return null;
+	}
+
+	@Override
+	public Type visit(StringValue stringValue) {
+		//TODO
+		return null;
+	}
+
+	@Override
+	public Type visit(VoidValue voidValue) {
+		//TODO
+		return null;
+	}
 }
